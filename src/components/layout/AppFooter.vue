bf8669db9be3e844f0fc3ab9fc934162f03591eb
Piero Narciso <pieronarciso988@gmail.com>
2020-03-29 20:41:15 -0500

    Add footer

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="app-footer"></div>
+  <div id="app-footer">
+    <v-footer app color="primary" dark>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} —
+        <strong>Cecata</strong>
+      </v-col>
+    </v-footer>
+  </div>
 </template>
 
 <script lang="ts">
