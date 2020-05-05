0eeb75aff36699e62f764c9264cedec33f1d0efb
Piero Narciso <pieronarciso988@gmail.com>
2020-05-05 12:05:40 -0500

    Footer absolute

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app-footer">
-    <v-footer app color="primary" dark>
+    <v-footer app color="primary" dark absolute>
       <v-col class="text-center" cols="12">
         <v-icon>mdi-copyright</v-icon>
         {{ new Date().getFullYear() }} —
