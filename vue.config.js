1dcce3bdf054bb7ea80f9b84ebc2dd5db0b673db
Piero Narciso <pieronarciso988@gmail.com>
2020-05-04 19:17:02 -0500

    settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'Covid-19',
+    themeColor: '#2196F3',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 }
